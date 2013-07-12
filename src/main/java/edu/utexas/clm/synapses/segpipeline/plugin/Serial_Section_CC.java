1dfe701ba53486f01f96d5b1b1a5c5c0f1d1c75e
Larry Lindsey <llindsey@clm.utexas.edu>
2013-07-11 19:02:18 -0500

    ProbImagetoConnectedComponents - rewritten to handle one image at a time, for distribution purposes

--- src/main/java/edu/utexas/clm/synapses/segpipeline/plugin/Serial_Section_CC.java
+++ src/main/java/edu/utexas/clm/synapses/segpipeline/plugin/Serial_Section_CC.java
@@ -1,20 +1,59 @@
 package edu.utexas.clm.synapses.segpipeline.plugin;
 
-import edu.utexas.clm.synapses.segpipeline.process.SerialSectionConnectedComponents;
+import edu.utexas.clm.synapses.segpipeline.process.ProbImageToConnectedComponents;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.plugin.PlugIn;
+import ij.process.FloatProcessor;
+import net.imglib2.img.ImagePlusAdapter;
+import net.imglib2.img.Img;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.numeric.RealType;
 
 
 public class Serial_Section_CC implements PlugIn
 
 {
     public void run(String s) {
-        final SerialSectionConnectedComponents sscc = new SerialSectionConnectedComponents(
-                IJ.getImage());
+        Img<? extends RealType> img;
+        ImagePlus imp = IJ.getImage();
+
+        switch(imp.getType())
+        {
+            case ImagePlus.GRAY8:
+                img = ImagePlusAdapter.wrapByte(imp);
+                break;
+            case ImagePlus.GRAY16:
+                img = ImagePlusAdapter.wrapShort(imp);
+                break;
+            case ImagePlus.GRAY32:
+                img = ImagePlusAdapter.wrapFloat(imp);
+                break;
+            default:
+                IJ.error("Must be a grayscale image");
+                return;
+        }
+
+        @SuppressWarnings("unchecked")
+        final ProbImageToConnectedComponents sscc = new ProbImageToConnectedComponents(img,
+                ProbImageToConnectedComponents.erodeDisk(1, img.firstElement()));
+        @SuppressWarnings("unchecked")
+        final ProbImageToConnectedComponents sscc2 = new ProbImageToConnectedComponents(img);
+
         if (sscc.checkInput() && sscc.process())
         {
-            new ImagePlus("Connected Components", sscc.getImageStack()).show();
+            ImageJFunctions.show(sscc.getLabel());
         }
+
+        if (sscc2.checkInput() && sscc2.process())
+        {
+            ImageJFunctions.show(sscc2.getLabel());
+        }
+
+        ProbImageToConnectedComponents.addToLabel(imp.getProcessor(), 100);
+
+        IJ.log("done");
+        imp.show();
+
     }
 }
--- src/main/java/edu/utexas/clm/synapses/segpipeline/process/ProbImageToConnectedComponents.java
+++ src/main/java/edu/utexas/clm/synapses/segpipeline/process/ProbImageToConnectedComponents.java
@@ -1,26 +1,36 @@
 package edu.utexas.clm.synapses.segpipeline.process;
 
 import ij.ImagePlus;
-import ij.ImageStack;
+import ij.process.FloatProcessor;
+import ij.process.ImageProcessor;
 import net.imglib2.Cursor;
 import net.imglib2.RandomAccess;
 import net.imglib2.algorithm.Algorithm;
 import net.imglib2.algorithm.Benchmark;
 import net.imglib2.algorithm.labeling.AllConnectedComponents;
+import net.imglib2.img.ImagePlusAdapter;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.labeling.NativeImgLabeling;
+import net.imglib2.ops.operation.UnaryOperation;
+import net.imglib2.ops.operation.randomaccessibleinterval.unary.morph.ErodeGray;
 import net.imglib2.type.logic.BitType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.IntType;
+import net.imglib2.type.numeric.real.FloatType;
 
-import java.awt.image.ColorModel;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.concurrent.*;
 
-public class ProbImageToConnectedComponents implements Algorithm, Benchmark
+
+/**
+ * Calculates a connected-components label image from an input image by thresholding it (after
+ * a preprocess step) to a given level, then running AllConnectedComponents. Provides a function
+ * to re-index the label image.
+ * @param <A>
+ */
+public class ProbImageToConnectedComponents<A extends RealType<A>> implements Algorithm, Benchmark
 {
     private static class IntTypeNamer implements Iterator<IntType>
     {
@@ -52,74 +62,33 @@ public class ProbImageToConnectedComponents implements Algorithm, Benchmark
         }
     }
 
-
-    public static <T extends RealType<T>> Img<IntType> connectedComponents(
-            final Img<T> img, final float threshold, final boolean gt, final float[] maxVal,
-            final int idx)
-    {
-        final long[] dimensions = new long[img.numDimensions()];
-        final IntTypeNamer namer = new IntTypeNamer(1);
-        Img<BitType> imgT;
-        Img<IntType> label;
-        NativeImgLabeling<IntType, IntType> labeling;
-
-        img.dimensions(dimensions);
-        label = new ArrayImgFactory<IntType>().create(dimensions, new IntType());
-        labeling = new NativeImgLabeling<IntType, IntType>(label);
-        imgT = new ArrayImgFactory<BitType>().create(dimensions, new BitType());
-
-
-        thresholdImg(img, imgT, threshold, gt);
-
-        AllConnectedComponents.labelAllConnectedComponents(
-                labeling,
-                imgT,
-                namer,
-                new long[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}});
-
-        maxVal[idx - 1] = namer.getLastValue();
-
-        return label;
-    }
-
-    public static <T extends RealType<T>> void addToLabel(Img<T> img, int addVal)
-    {
-        final Cursor<T> cursor = img.cursor();
-        T it;
-
-        while (cursor.hasNext())
-        {
-            cursor.fwd();
-            it = cursor.get();
-            if (it.getRealFloat() > 0)
-            {
-                it.setReal(it.getRealFloat() + addVal);
-            }
-        }
-    }
-
-
     private long pTime;
-    private final ExecutorService service;
     private String errorMsg;
-    private final ImagePlus imp;
+    private final Img<A> img;
     private float threshold;
     private boolean thresholdGT;
-    private long[] maxVal;
-    private ArrayList<Img<IntType>> ccImgs;
+    private final UnaryOperation<Img<A>, Img<A>> preProcess;
+    private long[] dim;
+    private Img<IntType> ccLabel;
+    private int maxVal;
 
-    public ProbImageToConnectedComponents(ImagePlus imp)
+    public ProbImageToConnectedComponents(final Img<A> img)
     {
-        this(imp, Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()));
+        this(img, new IdentityOperation<Img<A>>());
     }
 
-    public ProbImageToConnectedComponents(ImagePlus imp, ExecutorService service)
+    public ProbImageToConnectedComponents(final Img<A> img,
+                                          final UnaryOperation<Img<A>, Img<A>> preProcess)
     {
-        this.service = service;
-        this.imp = imp;
+        this.img = img;
+        this.preProcess = preProcess;
         errorMsg = "";
         threshold = 0.5f;
         thresholdGT = true;
+        dim = new long[img.numDimensions()];
+        img.dimensions(dim);
+        ccLabel = null;
+        maxVal = -1;
     }
 
     public void setThreshold(final float t, final boolean gt)
@@ -128,13 +97,14 @@ public class ProbImageToConnectedComponents implements Algorithm, Benchmark
         thresholdGT = gt;
     }
 
-
     public boolean checkInput()
     {
-        return imp.getType() != ImagePlus.COLOR_RGB && imp.getType() != ImagePlus.COLOR_256;
+        return true;
     }
 
-    public ArrayList<Future<Img<IntType>>> connectedComponentsFutures()
+
+
+    /*public ArrayList<Future<Img<IntType>>> connectedComponentsFutures()
     {
         final ImageStack is = imp.getImageStack();
         maxVal = new long[is.getSize()];
@@ -158,76 +128,37 @@ public class ProbImageToConnectedComponents implements Algorithm, Benchmark
         }
 
         return ccFutures;
-    }
+    } */
 
     public boolean process()
     {
         final long sTime = System.currentTimeMillis();
-        final ArrayList<Future<Img<IntType>>> ccFutures = connectedComponentsFutures();
-        final ArrayList<Future<Img<IntType>>> riFutures =
-                new ArrayList<Future<Img<IntType>>>(ccFutures.size() - 1);
-        ccImgs = new ArrayList<Img<IntType>>(ccFutures.size());
+        final Img<BitType> imgT = new ArrayImgFactory<BitType>().create(dim, new BitType());
+        ccLabel = new ArrayImgFactory<IntType>().create(dim, new IntType());
 
-        try
-        {
-            for (int i = 0; i < ccFutures.size(); ++i)
-            {
-                if (i == 0)
-                {
-                    ccImgs.add(ccFutures.get(i).get());
-                }
-                else
-                {
-                    riFutures.add(service.submit(new ReindexCallable(ccFutures.get(i).get(), i)));
-                }
-            }
-
-            for (Future<Img<IntType>> future : riFutures)
-            {
-                ccImgs.add(future.get());
-            }
-        }
-        catch (InterruptedException ie)
-        {
-            errorMsg = ie.toString();
-            return false;
-        }
-        catch (ExecutionException ee)
-        {
-            errorMsg = ee.toString();
-            return false;
-        }
+        thresholdImg(preProcess.compute(img, img.copy()), imgT, threshold, thresholdGT);
+        maxVal = connectedComponents(imgT, ccLabel);
 
         pTime = System.currentTimeMillis() - sTime;
         return true;
     }
     
-
-
-    public ImageStack getImageStack()
-    {
-        final ImageStack outputStack = new ImageStack(imp.getWidth(), imp.getHeight(),
-                ColorModel.getRGBdefault());
-        for (final Img<IntType> img : ccImgs)
-        {
-
-            outputStack.addSlice(ImageJFunctions.wrapFloat(img, "Image").getProcessor());
-        }
-        return outputStack;
-    }
-
-    public ArrayList<Img<IntType>> getImgs()
-    {
-        return new ArrayList<Img<IntType>>(ccImgs);
-    }
-
-
     public String getErrorMessage() {
         return errorMsg;
     }
 
     public long getProcessingTime() {
         return pTime;
+    }
+
+    public int getMaxVal()
+    {
+        return maxVal;
+    }
+
+    public Img<IntType> getLabel()
+    {
+        return ccLabel;
     }
 
     public static <T extends RealType> void thresholdImg(Img<T> fimg, Img<BitType> bimg,
@@ -248,4 +179,87 @@ public class ProbImageToConnectedComponents implements Algorithm, Benchmark
             target.get().set(test == gt);
         }
     }
+
+    public static int connectedComponents(final Img<BitType> img, final Img<IntType> label)
+    {
+        final IntTypeNamer namer = new IntTypeNamer(1);
+        final NativeImgLabeling<IntType, IntType> labeling =
+                new NativeImgLabeling<IntType, IntType>(label);
+
+        AllConnectedComponents.labelAllConnectedComponents(
+                labeling,
+                img,
+                namer,
+                new long[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}});
+
+        return namer.getLastValue();
+    }
+
+    public static <T extends RealType<T>> void addToLabel(Img<T> img, int addVal)
+    {
+        final Cursor<T> cursor = img.cursor();
+        T it;
+
+        while (cursor.hasNext())
+        {
+            cursor.fwd();
+            it = cursor.get();
+            if (it.getRealFloat() > 0)
+            {
+                it.setReal(it.getRealFloat() + addVal);
+            }
+        }
+    }
+
+    public static void addToLabel(ImageProcessor ip, int addVal)
+    {
+        int v;
+        for (int x = 0; x < ip.getWidth(); ++x)
+        {
+            for (int y = 0; y < ip.getHeight(); ++y)
+            {
+                v = ip.get(x, y);
+                if (v > 0)
+                {
+                    ip.set(x, y, v + addVal);
+                }
+            }
+        }
+    }
+
+    private static long[][] diskStrel(final int r)
+    {
+        long[][] strel;
+        final ArrayList<long[]> v = new ArrayList<long[]>();
+        final int r2 = r*r;
+
+        for (int i = 0; i <= r; ++i)
+        {
+            for (int j = 0; j <= r; ++j)
+            {
+                if (i*i + j*j <= r2)
+                {
+                    v.add(new long[]{i,j});
+                    v.add(new long[]{-i,j});
+                    v.add(new long[]{i,-j});
+                    v.add(new long[]{-i,-j});
+                }
+            }
+        }
+
+        strel = new long[v.size()][];
+
+        for (int i = 0; i < v.size(); ++i)
+        {
+            strel[i] = v.get(i);
+        }
+
+        return strel;
+    }
+
+    public static <T extends RealType<T>> ErodeGray<T, Img<T>> erodeDisk(int r, T type)
+    {
+        return new ErodeGray<T, Img<T>>(diskStrel(r));
+    }
+
 }
